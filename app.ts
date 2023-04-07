ed18214ade92aa404029072eb02ed9b448809f26
GABRIELLY MALDONADO DORNELES <49543498+GMDorneles@users.noreply.github.com>
2023-04-06 23:26:46 -0400

    feat: login route and validations

--- app.ts
+++ app.ts
@@ -59,14 +59,43 @@ app.post("/auth/register", async (req, res) => {
 
   try {
     await user.save();
-    res.status(201).json({ msg: "Usuário criado com sucesso!" });
+    res.status(201).json({ msg: "User successfully created!" });
   } catch (err) {
     res.status(500).json({
-      msg: "Aconteceu um erro no servidor, tente novamente mais tarde!",
+      msg: "There was a server error, please try again later!",
     });
   }
 });
 
+//login user
+
+app.post("/auth/login", async (req, res) => {
+  const { email, password } = req.body;
+
+  //validations
+  if (!email) {
+    return res.status(422).json({ msg: "Email is required" });
+  }
+
+  if (!password) {
+    return res.status(422).json({ msg: "Password is required" });
+  }
+
+  //check if user exists
+  const user = await User.findOne({ email: email });
+
+  if (!user) {
+    return res.status(404).json({ msg: "The email is not registered" });
+  }
+
+  //check if password match
+  const checkPassword = await bcrypt.compare(password, user.password);
+
+  if (!checkPassword) {
+    return res.status(422).json({ msg: "Invalid password" });
+  }
+});
+//credentials
 const dbUser = process.env.DB_USER;
 const dbPassword = process.env.DB_PASS;
 
