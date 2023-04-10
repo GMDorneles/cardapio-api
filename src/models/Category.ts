2a4e179cd91164a2f7becc8f9442fe2b7ed70230
GABRIELLY MALDONADO DORNELES <49543498+GMDorneles@users.noreply.github.com>
2023-04-10 02:14:31 -0400

    fix: category model error

--- src/models/Category.ts
+++ src/models/Category.ts
@@ -4,12 +4,6 @@ const Category = mongooseCategory.model(
   "Category",
   new mongooseCategory.Schema({
     name: String,
-    Products: [
-      {
-        type: mongooseCategory.Schema.Types.ObjectId,
-        ref: "Product",
-      },
-    ],
   })
 );
 
